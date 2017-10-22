69a0c8fd25bbe5d60a3c9f57400072b2c67b35c8
Raquel Domingo <raqueldomingo@Raquel-Domingo.local>
2017-10-22 16:48:22 -0700

    Initial commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { AppPage } from './app.po';
+import { RegistrationPage } from './app.po';
 
 describe('registration App', () => {
-  let page: AppPage;
+  let page: RegistrationPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new RegistrationPage();
   });
 
   it('should display welcome message', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { EqualValidator } from './equal-validator.directive';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EqualValidator
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
